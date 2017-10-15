125e6d1aee2d68b85b6e30d57e00dcaf1d04a331
Rodrigo Fontes <fontesr2@gmail.com>
2017-10-15 14:12:51 -0200

    Move struct definition to header
    The compiler was throwing an error saying the struct definition was
    incomplete.

--- str_calculator/node.c
+++ str_calculator/node.c
@@ -1,27 +1,5 @@
 #include "node.h"
 
-struct SNode {
-
-    long m_lValue;
-
-    struct SNode * m_pnParent;
-    struct SNode * m_pnLChild;
-    struct SNode * m_pnRChild;
-
-    long (* m_lGetValue)(struct SNode * this);
-    struct SNode * (* m_pnGetParent)(struct SNode * this);
-    struct SNode * (* m_pnGetLChild)(struct SNode * this);
-    struct SNode * (* m_pnGetRChild)(struct SNode * this);
-
-    void (* m_setValue)(struct SNode * this, long lValue);
-    void (* m_setParent)(struct SNode * this, struct SNode * pnParent);
-    void (* m_setLChild)(struct SNode * this, struct SNode * pnLChild);
-    void (* m_setRChild)(struct SNode * this, struct SNode * pnRChild);
-
-    void (* m_destroy)(struct SNode * this);
-    void (* m_construct)(struct SNode * this);
-};
-
 static long lGetValue(struct SNode * this) {
     return this->m_lValue;
 }
