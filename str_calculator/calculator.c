ac3ce0be231be6fd6a615e7b5ec59816c5634839
Rodrigo Prado Fontes <fontesr2@gmail.com>
2017-11-10 16:56:47 -0200

    Add first draft for parsing parentheses

--- str_calculator/calculator.c
+++ str_calculator/calculator.c
@@ -27,7 +27,7 @@ static void handleTreeNodes(long lNodeVal) {
     long lOperand2;
 
     if (lNodeVal < 0) {
-        // If lNodeVal is operation, perform it and store result
+        // If lNodeVal is an operation, perform it and store result
         lOperand2 = stOps.m_pop(&stOps);
         lOperand1 = stOps.m_pop(&stOps);
         stOps.m_lPush(&stOps, lPerformOp(lOperand1, lOperand2, lNodeVal));
@@ -73,13 +73,56 @@ static long lStr2num(char * szStr, long * lIdx) {
     return lNum;
 }
 
+// This currrently works only if the expression has one opening parenthesis that is not at index 0 and one closing parenthesis at the last position
+static void handleParentheses(struct SCalcBTree * tCalc, struct Stack * stTreeNodes, char * szExpression, long * lIdx) {
+
+    struct SNode * root;
+    struct SNode * curr;
+    struct SNode * par;
+
+    while (szExpression[*lIdx] == '(') {
+
+        root = tCalc->m_pnGetRoot(tCalc);
+        curr = tCalc->m_pnGetCurrNode(tCalc);
+        par = tCalc->m_pnGetCurrPar(tCalc);
+
+        stTreeNodes->m_lPush(stTreeNodes, par->m_lGetIdx(par));
+        stTreeNodes->m_lPush(stTreeNodes, root->m_lGetIdx(root));
+
+        par->m_setRChild(par, NULL);
+        curr->m_setParent(curr, NULL);
+
+        tCalc->m_setRoot(tCalc, curr);
+
+        (*lIdx)++;
+    }
+
+    while (szExpression[*lIdx] == ')') {
+
+        root = tCalc->m_pnGetNodeByIdx(tCalc, stTreeNodes->m_pop(stTreeNodes));
+        curr = tCalc->m_pnGetRoot(tCalc);
+        par = tCalc->m_pnGetNodeByIdx(tCalc, stTreeNodes->m_pop(stTreeNodes));
+
+        par->m_setRChild(par, curr);
+        curr->m_setParent(curr, par);
+
+        tCalc->m_setRoot(tCalc, root);
+
+        (*lIdx)++;
+    }
+}
+
 static void formTree(struct SCalcBTree * tCalc, char * szExpression, unsigned int uiExpSize) {
 
     long lIdx = 0;
     long lNum;
     enum opearations op;
 
+    struct Stack stTreeNodes = newStack(tCalc->m_uiNodeQtt);
+
     while (lIdx < uiExpSize && szExpression[lIdx] != '\0') {
+
+        handleParentheses(tCalc, &stTreeNodes, szExpression, &lIdx);
 
         lNum = lStr2num(szExpression, &lIdx);
 
@@ -87,7 +130,11 @@ static void formTree(struct SCalcBTree * tCalc, char * szExpression, unsigned in
 
         tCalc->m_placeOp(tCalc, op, lNum);
 
-        lIdx++;
+        if (szExpression[lIdx] == ')') {
+            handleParentheses(tCalc, &stTreeNodes, szExpression, &lIdx);
+        } else {
+            lIdx++;
+        }
     }
 }
 
